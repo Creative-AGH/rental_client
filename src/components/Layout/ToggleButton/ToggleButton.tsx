d5497f1b4eb623dc00a63713d2545e4458198a30
Adam Jasinski <admjasinski@gmail.com>
2022-09-19 10:32:47 +0200

    fix: toggle button functionality

--- src/components/Layout/ToggleButton/ToggleButton.tsx
+++ src/components/Layout/ToggleButton/ToggleButton.tsx
@@ -4,9 +4,9 @@ import { toggleButtonT } from '../../../types/ThemeT';
 
 const ToggleButton = ({ onClick }: toggleButtonT) => {
     return (
-        <div id={styles.darkmode} onClick={onClick}>
-            <input type="checkbox" className={styles.checkbox} id={styles.checkbox} />
-            <label htmlFor="checkbox" className={styles.label}>
+        <div className={styles.darkmode} >
+            <input type="checkbox" className={styles.checkbox} onClick={onClick} />
+            <label htmlFor="checkbox" className={styles.label} >
                 <BsFillMoonFill />
                 <BsFillSunFill color='white' />
                 <div className={styles.ball}></div>
